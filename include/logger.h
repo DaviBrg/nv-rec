7ea98f8b1e999f3549b81317ae4b6069630b17ff
Davi Braga <davibrg@gmail.com>
2018-09-26 14:51:21 -0300

    Added group commit

--- include/logger.h
+++ include/logger.h
@@ -2,6 +2,7 @@
 #define LOGGER_H
 
 #include <chrono>
+#include <list>
 #include <vector>
 #include <string>
 #include <thread>
@@ -9,6 +10,8 @@
 #include <mutex>
 #include <future>
 #include <condition_variable>
+#include <atomic>
+#include <memory>
 
 namespace nvrec {
 
@@ -19,23 +22,50 @@ enum CommitStatus {
 };
 typedef std::pair<std::string,std::string>  KVPair;
 
+class TxJob {
+public:
+    TxJob(std::string table, std::string key, std::vector<KVPair> values):
+        table_(std::move(table)),
+        key_(std::move(key)),
+        values_(std::move(values)) {}
+    std::string& table() {return table_;}
+    std::string& key() {return key_;}
+    std::vector<KVPair>& values() {return values_;}
+private:
+    std::string table_;
+    std::string key_;
+    std::vector<KVPair> values_;
+};
+
+
 class Logger {
 public:
-    Logger(const std::string &file_name, size_t frequency, size_t transactions);
+    Logger(const std::string &file_name, size_t tx_timeout, size_t tx_limit);
     ~Logger();
     CommitStatus Commit(const std::string &table, const std::string &key,
                         const std::vector<KVPair> &values);
 private:
-    void WriteToBuffer(const std::string &table, const std::string &key,
-                       const std::vector<KVPair> &values, std::promise<CommitStatus> &&commit_promise);
-    size_t frequency_, transactions_, tx_counter_ = 0;
+    typedef std::vector<std::promise<CommitStatus>> CommitPromises;
+    typedef std::vector<TxJob> CommitJobs;
+    class CommitGroup {
+    public:
+        CommitGroup(CommitPromises in_promises, CommitJobs in_jobs):
+            promises(std::move(in_promises)),
+            jobs(std::move(in_jobs)){}
+        CommitPromises promises;
+        CommitJobs jobs;
+    };
+    void AtomicWriteToFile(const std::string &table, const std::string &key,
+                       const std::vector<KVPair> &values);
+    void AtomicFlush();
+    void ExecuteJobs(CommitGroup &cg);
+    void KeepPromises(CommitGroup &cg);
+    size_t tx_timeout_, txs_limit_;
     std::ofstream file_;
-    std::mutex m_;
-    std::vector<std::promise<CommitStatus>> pending_promises_;
-    std::condition_variable commit_cv_;
-    bool notified_ = false;
+    std::mutex commit_m_, file_m_;
+    std::condition_variable group_commit_cv_, empty_cv;
+    std::list<CommitGroup> commit_list_;
     std::thread logger_;
-
 };
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include "logger.h"
 
+#include <iostream>
+
 int main(int argc, char *argv[]) {
-    nvrec::Logger lg("test.txt", 3, 100);
-    lg.Commit("tabela", "chave", {{"nome","davi"},{"idade","28"},{"profissao","dev"}});
+    nvrec::Logger lg("test.txt", 3000, 5);
+//    lg.Commit("tabela", "chave", {{"nome","davi"},{"idade","28"},{"profissao","dev"}});
+
+    std::vector<std::thread> threads;
+
+    for (size_t i=0; i<1; i++) {
+        std::vector<nvrec::KVPair> pairs = {{"nome","davi"},{"idade","28"},{"profissao","dev"}};
+        threads.emplace_back(std::thread(&nvrec::Logger::Commit, &lg, "tabela", "chave", std::move(pairs)));
+    }
+
+    for (auto &th : threads) {
+        th.join();
+    }
+
+    std::cout << "Finished" << std::endl;
+
     return 0;
 }
