eaaf3b0c68190e9efaaf6507b3a344035e7b204d
Davi Braga <davibrg@gmail.com>
2018-11-12 15:37:08 -0300

    Addded protobuf generated files

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,12 @@
-#include "logger.h"
-
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
-int main(int argc, char *argv[]) {
-    nvrec::Logger lg("test.txt", 3000, 5);
-//    lg.Commit("tabela", "chave", {{"nome","davi"},{"idade","28"},{"profissao","dev"}});
+using std::cout;
 
-    std::vector<std::thread> threads;
+int main(int argc, char argv[]) {
+  cout << " Hello World!\n";
 
-    for (size_t i=0; i<1; i++) {
-        std::vector<nvrec::KVPair> pairs = {{"nome","davi"},{"idade","28"},{"profissao","dev"}};
-        threads.emplace_back(std::thread(&nvrec::Logger::Commit, &lg, "tabela", "chave", std::move(pairs)));
-    }
 
-    for (auto &th : threads) {
-        th.join();
-    }
-
-    std::cout << "Finished" << std::endl;
-
-    return 0;
+  return 0;
 }
